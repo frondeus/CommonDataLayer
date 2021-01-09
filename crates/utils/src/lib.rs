d9a240c30e45af2ea4f797f8ef6234ff5c649481
Kononnable <kononnable@gmail.com>
2021-01-10 00:16:59 +0100

    task queue

--- crates/utils/src/lib.rs
+++ crates/utils/src/lib.rs
@@ -8,6 +8,7 @@ pub mod psql;
 pub mod query_utils;
 pub mod status_endpoints;
 pub mod task_limiter;
+pub mod task_queue;
 
 pub fn abort_on_poison<T>(_e: PoisonError<T>) -> T {
     error!("Encountered mutex poisoning. Aborting.");
---
+++ crates/utils/src/task_queue.rs
@@ -0,0 +1,88 @@
+use std::{collections::{HashMap, VecDeque}, sync::{Arc, Mutex}};
+
+use tokio::sync::oneshot::{channel, Receiver, Sender};
+
+// TODO: remove unwraps
+// TODO: change name
+// TODO: make generic
+
+type LocksDB = Arc<Mutex<HashMap<String, LockQueue>>>;
+
+pub struct TaskQueue {
+    locks: LocksDB
+}
+
+impl TaskQueue{
+    pub async fn add_task(&self,key:String)->LockGuard{
+        let receiver = {
+            let mut locks = self.locks.lock().unwrap();
+            let lock_queue= LockQueue::new();
+            let queue = (*locks).entry(key.clone()).or_insert(lock_queue);
+            queue.add_task()
+        };
+        receiver.await.unwrap();
+
+        LockGuard{
+            db:self.locks.clone(),
+            key
+        }
+    }
+}
+
+pub struct LockQueue {
+    is_blocked: bool,
+    queue: VecDeque<Sender<()>>,
+}
+
+impl LockQueue {
+    fn new()->LockQueue{
+        LockQueue{
+            is_blocked:false,
+            queue:VecDeque::new()
+        }
+    }
+
+    fn add_task(&mut self) -> Receiver<()> {
+        let (tx, rx) = channel();
+        match self.is_blocked {
+            true => {
+                self.queue.push_back(tx);
+            }
+            false => {
+                self.is_blocked = true;
+                tx.send(()).expect("Receiver dropped");
+            }
+        };
+        rx
+    }
+
+    fn end_task(&mut self) {
+        match self.queue.pop_front() {
+            None => {
+                self.is_blocked = false;
+            }
+            Some(tx) => {
+                let result = tx.send(());
+                if result.is_err() {
+                    // receiver dropped, start next task
+                    self.end_task()
+                }
+            }
+        }
+    }
+}
+
+pub struct LockGuard {
+    db: LocksDB,
+    key: String
+}
+impl Drop for LockGuard {
+    fn drop(&mut self) {
+        let mut guard = self.db.lock().unwrap();
+        let entry =(*guard).get_mut(&self.key).unwrap();
+        entry.end_task();
+        if !entry.is_blocked{
+            (*guard).remove(&self.key);
+        }
+    }
+}
